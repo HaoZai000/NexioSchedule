445bbb408ea48618ab1c5284f18ee833b1194e51
haooz <com_haooz_account@users.noreply.gitee.com>
2026-07-09 01:29:02 +0800

    auto-backup: 2026/07/09 周四  1:29:02.09

--- temp_extract2.ps1
+++
@@ -1,6 +0,0 @@
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-$src = 'C:\Users\43908\.gradle\caches\9.5.1\transforms\942176d5cf977e560a63e5aab375df8a\transformed\miuix-blur-api.jar'
-$dst = 'C:\Users\43908\Desktop\KeCB\temp_miuix_blur_api'
-if (Test-Path $dst) { Remove-Item $dst -Recurse -Force }
-[System.IO.Compression.ZipFile]::ExtractToDirectory($src, $dst)
-Get-ChildItem $dst -Recurse -Filter '*.class' | ForEach-Object { $_.FullName }
