c876d5d0dab16e7334b7a725ea8a3742208dbbb1
haooz <com_haooz_account@users.noreply.gitee.com>
2026-08-24 21:09:16 +0800

    v1.4.4b1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.haooz.chedule"
         minSdk = 31
         targetSdk = 37
-        versionCode = 142
-        versionName = "1.4.2-0825"
+        versionCode = 143
+        versionName = "1.4.3-0825"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
